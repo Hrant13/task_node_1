3cfabdc5c1ae37e47da0af10b5720f468a23d3f8
Hrant Tovmasyan <78748319+Hrant13@users.noreply.github.com>
2021-12-30 00:21:19 +0400

    Update index.js

--- index.js
+++ index.js
@@ -17,3 +17,18 @@ function map(arr, callback){
  }
 let result = map([2,3], (el)=>{return(el*2)})
 console.log(result);
+
+
+//  implementation of slice() function
+
+function newSlice(arr, start, end = arr.length) {
+  let newSlisedArr = [];
+  // if (end === undefined || end > arr.length) {
+  //   end = arr.length;
+  // }
+  for (let index = start; index < end ; index++) {
+    newSlisedArr.push(arr[index]);
+  }
+  return newSlisedArr;
+}
+ console.log(newSlice([1,2,3,4,5,6,7],3,5));
